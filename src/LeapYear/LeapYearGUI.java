738cf96bb4bf33723a8170974ae9ac075037a480
Xahtrin <matthew.1313@yahoo.com>
2022-12-18 01:20:07 -0800

    Add LeapYear main function (failed attempt)

--- src/LeapYear/LeapYearGUI.java
+++ src/LeapYear/LeapYearGUI.java
@@ -2,11 +2,24 @@ package LeapYear;
 
 import javax.swing.*;
 
-public class LeapYearGUI {
+public class LeapYearGUI extends JFrame{
     private JPanel panel1;
     private JTextField tfYear;
     private JButton btnCheckYear;
     private JLabel input;
+
+    public static void main (String args[]) {
+        LeapYearGUI app = new LeapYearGUI();
+        app.setContentPane(app.panel1);
+        app.setSize(700, 700);
+        app.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        app.setVisible(true);
+    }
+
+    public LeapYearGUI() {
+        System.out.println("HELLO");
+
+    }
 
 
     private void createUIComponents() {
