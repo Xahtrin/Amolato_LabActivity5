17c2624383a23a1399fcdbd1799998639fbe4093
Xahtrin <matthew.1313@yahoo.com>
2022-12-24 01:49:25 -0800

    Add SimpleCalc main function

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -1,12 +1,24 @@
 package SimpleCalc;
 
+import LeapYear.LeapYearGUI;
+
 import javax.swing.*;
 
-public class SimpleCalcGUI {
+public class SimpleCalcGUI extends JFrame{
     private JPanel panel1;
     private JTextField tfNumber1;
     private JComboBox cbOperations;
     private JButton btnCompute;
     private JTextField tfNumber2;
     private JLabel lblResult;
+
+    public static void main (String args[]) {
+
+        SimpleCalcGUI app = new SimpleCalcGUI();
+        app.setTitle("Leap Year Checker");
+        app.setContentPane(app.panel1);
+        app.setSize(700, 700);
+        app.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        app.setVisible(true);
+    }
 }
