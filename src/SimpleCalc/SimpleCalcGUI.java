977de2b1b342da4d5e6b665e75e4178d7efcbd7a
Xahtrin <matthew.1313@yahoo.com>
2022-12-24 02:17:20 -0800

    Complete SimpleCalc

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -3,6 +3,8 @@ package SimpleCalc;
 import LeapYear.LeapYearGUI;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class SimpleCalcGUI extends JFrame{
     private JPanel panel1;
@@ -11,6 +13,37 @@ public class SimpleCalcGUI extends JFrame{
     private JButton btnCompute;
     private JTextField tfNumber2;
     private JLabel lblResult;
+    private JTextField tfResult;
+
+    public SimpleCalcGUI() {
+        btnCompute.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                double a = Integer.parseInt(tfNumber1.getText());
+                double b = Integer.parseInt(tfNumber2.getText());
+                int op = cbOperations.getSelectedIndex();
+                double res;
+                switch (op) {
+                    case 0:
+                        res = a + b;
+                        tfResult.setText(String.valueOf(res));
+                        break;
+                    case 1:
+                        res = a - b;
+                        tfResult.setText(String.valueOf(res));
+                        break;
+                    case 2:
+                        res = a * b;
+                        tfResult.setText(String.valueOf(res));
+                        break;
+                    case 3:
+                        res = a / b;
+                        tfResult.setText(String.valueOf(res));
+                        break;
+                }
+            }
+        });
+    }
 
     public static void main (String args[]) {
 
